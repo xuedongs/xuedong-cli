1d6c258f2c386e52778c50806c463edb8587f7da
孙雪冬 <sunxd@hite.com.cn>
2024-05-16 18:11:49 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
+    },
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "@typescript-eslint/ban-ts-ignore": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "no-var": "error",
+    "prettier/prettier": "error",
+    "no-console": "error",
+    "no-debugger": "error",
+  },
+};
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # xuedong-cli
+
+monorepo with pnpm
+
+pnpm recursive install
+
+## Use Cases
+
+#### link package globally
+
+pnpm link --global
+
+### link foo to my-project
+
+pnpm link --global package
+
+### remove a link made with pnpm link --global <package>
+
+pnpm uninstall --global package
+
+### removes unnecessary packages
+
+pnpm prune
+
+pnpm -F 子包名 add 需要下载的包名
