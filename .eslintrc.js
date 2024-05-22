22f8b2f96d360cc7b5fb9e1c2c75c5c2db95f42c
孙雪冬 <sunxd@hite.com.cn>
2024-05-22 15:42:10 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  root: true,
+  // root: true,
   env: {
     browser: true,
     node: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended", "prettier"],
+  extends: ["eslint:recommended", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
